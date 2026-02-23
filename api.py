9ce849829a2fe64f9ba0dddb3a3214dc644d4242
Harish R <73359159+Harish1192@users.noreply.github.com>
2026-02-23 10:00:48 +0530

    Comment out main execution block in api.py
    Comment out the main execution block for the API.

--- api.py
+++ api.py
@@ -100,7 +100,7 @@ def predict(request: URLRequest):
   )
 
 
-if __name__=="__main__":
-  import uvicorn
-  uvicorn.run("api:app",host="0.0.0.0",port=8000,reload=True)
+# if __name__=="__main__":
+#   import uvicorn
+#   uvicorn.run("api:app",host="0.0.0.0",port=8000,reload=True)
 
